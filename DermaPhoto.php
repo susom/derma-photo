c79986206ff569860a98a9abad6cb6f3552d7c79
Irvin Szeto <irvins@stanford.edu>
2021-12-21 14:49:11 -0800

    removign the ui dialog box ...

--- DermaPhoto.php
+++ DermaPhoto.php
@@ -27,25 +27,31 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
 	}
 
     public function redcap_survey_page_top($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance) {
-        $this->emDebug("Hello Newman");
+        $file_upload_page   = APP_PATH_SURVEY . "index.php?pid=" . PROJECT_ID . "&__passthru=".urlencode("DataEntry/file_upload.php");
+        $file_empty_page    = APP_PATH_SURVEY . "index.php?pid=" . PROJECT_ID . "&__passthru=".urlencode("DataEntry/empty.php") . '&s=' . $_GET['s'];
+        $formAction         = $file_upload_page.'&id='.rawurlencode($_GET['id']).'&event_id='.$_GET['event_id'].'&instance='.$_GET['instance'].'&s='.$_GET['s'];
+
+        $file_upload_win2 = '<form autocomplete="new-password" id="form_file_upload" action="'.$formAction.'" method="post" enctype="multipart/form-data" target="upload_target" onsubmit="return startUpload();" >
+                                <div id="f1_upload_process"></div>
+                                <div id="f1_upload_form" style="">
+                                    <input name="myfile" type="file" size="40">
+                                    <input name="myfile_base64" type="hidden">
+                                    <input name="myfile_base64_edited" type="hidden" value="0">
+                                    <input name="myfile_replace" type="hidden" value="0">
+                                    <button class="btn btn-primaryrc btn-fileupload" onclick="uploadFilePreProcess();return false;"><i class="fas fa-upload"></i> Upload file</button>
+                                </div>
+                                <input type="hidden" id="field_name" name="field_name" value="">
+                                <iframe id="upload_target" name="upload_target" src="'.$file_empty_page.'" ></iframe>
+                            </form>';
         ?>
         <script>
             $(document).ready(function() {
-                // console.log('hi');
-
-                //Add javascript here...
-                //	    $("a.fileuploadlink").text('Take photo');
-                // Finish loading ImageMap
-
                 setTimeout(function() {
-                    // console.log('300');
                     FU.init();
                 }, 300);
-
             });
 
             var FU = {
-
                 currentData: {},
 
                 uploadClosed: function() {
@@ -64,32 +70,18 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
 
                         if (uploadField.val()) {
                             // We have a file - make sure it is still checked
-
                             if (!checked) {
                                 console.log('image but not checked - check');
-                                // cbx.click().trigger('onclick');
                                 var p = $(cbx).parent();
                                 console.log('p', p);
                                 p.trigger('click');
-
-                                // imageMapEM.updateAreaList(imageMapEM, FU.currentData);
                             }
-
                         }
                         else {
                             // Make sure checkbox is NOT checked
                             if (checked) {
                                 console.log('no image but checked - uncheck');
-
                                 $('area[data-key="' + key + '"]').click();
-
-
-                                // cbx.click().trigger('onclick');
-                                // var p =  $(cbx).parent();
-                                // console.log('p',p);
-                                // p.trigger('onclick');
-                                //                    p.triggerHandler('click');
-                                // imageMapEM.updateAreaList(imageMapEM, FU.currentData);
                             }
 
                         }
@@ -101,10 +93,10 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
                 },
 
                 init: function() {
-
                     // Capture close event
                     $("#file_upload").on("dialogclose", function(event, ui) {
-                        FU.uploadClosed();
+                        console.log("no dialog no close!");
+                        // FU.uploadClosed();
                     });
 
                     //as soon as the photo has been uploaded, click the Upload button
@@ -127,15 +119,7 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
                         $('input[type=checkbox][code="' + data.key + '"]', tr).each(function() {
                             imageMapEM.log('Found checkbox ' + data.key);
                             var checked = $(this).is(":checked");
-                            //imageMapEM.log ('is checked: ' + checked);
                             var selected = data.selected;
-                            //imageMapEM.log ('is selected: ' + selected);
-                            if (checked !== selected) {
-
-                                //commenting this out as broken after upgrade to 10.5.1
-                                //$(this).click().trigger('onclick');
-                                //$(this).blur();
-                            }
                         });
 
                         FU.currentData = data;
@@ -143,12 +127,16 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
                         // Does field data.key exist?
                         var uploadField = $('input[name="' + data.key + '"]');
                         console.log('uploadField for ' + data.key, uploadField);
+
                         if (uploadField.length && data.selected) {
                             // Found the field
                             imageMapEM.log("Found " + data.key);
 
                             // TRIGGER UPLOAD
-                            filePopUp(data.key, 0);
+                            //MODIFY BEHAVIOUR HERE
+                            // console.log("what is data", data);
+                            noUIfilePopUp(data.key, 0);
+
                             // PRESS FIND FILE BUTTON AUTOMATICALLY
                             $('input[name="myfile"]').trigger('click');
                             //HIDE the select file button
@@ -166,6 +154,24 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
 
                 }
 
+            }
+
+            function noUIfilePopUp(field_name, signature, replace_version){
+                // filePopUp(field_name, 0);
+                minPopUpBackUp(field_name, 0);
+            }
+
+            function minPopUpBackUp(field_name, signature, replace_version) {
+                // Reset value of hidden field used to determine if signature was signed
+                $('#f1_upload_form input[name="myfile_base64_edited"]').val('0');
+                $('#f1_upload_form input[name="myfile_replace"]').val(replace_version);
+
+                var file_upload_win = '<?php echo js_escape($file_upload_win2) ?>';
+
+                document.getElementById('file_upload').innerHTML = file_upload_win;
+                document.getElementById('field_name').value = field_name+'-linknew';
+
+                $('#f1_upload_form').show();
             }
         </script>
         <?php
--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 
 External Modularizing the shazam JS from original iteration
 
+
+            // const newfilePopUp = (() => {
+            //     let initFunctions = [];
+            //     return {
+            //         init() {
+            //             const fns = initFunctions;
+            //             initFunctions = undefined;
+            //             for (const fn of fns) {
+            //                 try { fn(); } catch (e) { }
+            //             }
+            //         },
+            //         addInitFunction(fn) {
+            //             if (initFunctions) {
+            //                 // Init hasn't run yet, remember it
+            //                 initFunctions.push(fn);
+            //             } else {
+            //                 // `init` has already run, call it almost immediately
+            //                 // but *asynchronously* (so the caller never sees the
+            //                 // call synchronously)
+            //                 setTimeout(fn, 0);
+            //                 // Or: `Promise.resolve().then(() => fn());`
+            //                 // (Not `.then(fn)` just to avoid passing it an argument)
+            //             }
+            //         }
+            //     };
+            // })();
