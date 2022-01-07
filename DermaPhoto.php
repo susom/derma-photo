6a3ceed834292f2f189189421fbf53635b97d835
Irvin Szeto <irvins@stanford.edu>
2022-01-07 08:28:54 -0800

    smooth out some of the upload UI/UX so when canceling an upload, UI gets reset

--- DermaPhoto.php
+++ DermaPhoto.php
@@ -80,14 +80,16 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
                 init: function() {
                     // Capture close event
                     $("#file_upload").on("dialogclose", function(event, ui) {
-                        console.log("no dialog no close!");
                         FU.uploadClosed();
                     });
 
                     //as soon as the photo has been uploaded, click the Upload button
                     $('body').on("change", 'input[name = "myfile"]', function() {
-                        console.log("trigger button");
+                        console.log("auto trigger upload button");
                         $('#f1_upload_form .btn-fileupload').trigger('click');
+
+                        //set flag that it is uploading
+                        window.uploadflag = true;
                     });
 
 
@@ -103,33 +105,49 @@ class DermaPhoto extends \ExternalModules\AbstractExternalModule {
                         // DEFAULT CHECKBOX BEHAVIOR
                         $('input[type=checkbox][code="' + data.key + '"]', tr).each(function() {
                             imageMapEM.log('Found checkbox ' + data.key);
-                            var checked = $(this).is(":checked");
-                            var selected = data.selected;
+                            var checked     = $(this).is(":checked");
+                            var selected    = data.selected;
                         });
 
                         FU.currentData = data;
 
                         // Does field data.key exist?
                         var uploadField = $('input[name="' + data.key + '"]');
-                        console.log('uploadField for ' + data.key, uploadField);
+                        // console.log('uploadField for ' + data.key, uploadField);
 
                         if (uploadField.length && data.selected) {
                             // Found the field
                             imageMapEM.log("Found " + data.key);
 
                             // TRIGGER UPLOAD
-                            //MODIFY BEHAVIOUR HERE
+                            //open up default REDCAP upload dialog UI
                             filePopUp(data.key, 0);
+
+                            //set uploading flag to false for every new modal
+                            window.uploadflag = false;
+
+                            //on input[myfile] click , native finder ui opens and entire document loses focus
+                            //when finder dialog is closed (due to canceling or file selection) focus returns to document, act if canceled.
+                            $('input[name="myfile"]').on("click", function(){
+                                document.body.onfocus = function(){
+                                    setTimeout(function(){
+                                        if(!window.uploadflag){
+                                            //dehighlight area
+                                            $("area[data-key='"+data.key+"']").mapster('deselect');
+                                            //auto trigger close button
+                                            $("button.ui-dialog-titlebar-close").trigger("click");
+                                        }
+                                    }, 1000);
+                                }
+                            });
 
                             // PRESS FIND FILE BUTTON AUTOMATICALLY
                             $('input[name="myfile"]').trigger('click');
+
                             //HIDE the select file button
                             $('input[name="myfile"]').hide();
                             //hide the uplaode button
                             $('#f1_upload_form .btn-fileupload').hide();
-
-
-                            // return;
                         }
                         else {
                             imageMapEM.log("Cannot find field for " + data.key);
